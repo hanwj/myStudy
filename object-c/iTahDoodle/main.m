3ca856efec41c3550f154c0b25064fab09a5c66d
hanwujie <hanwujie@airwind.cn>
2015-07-07 01:35:56 +0800

    fixbug

---
+++ object-c/iTahDoodle/main.m
@@ -0,0 +1,7 @@
+#import <Foundation/Foundataion.h>
+#import "BNRAppDelegate.h"
+
+int main(int argv,char *argv)
+{
+	return UIApplicationMain(argc,argv,nil,NSStringFromClass([BNRAppDelegate class]));
+}
