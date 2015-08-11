3f7cc277194e761a907c1204fef487440cb3ad49
hanwujie <hanwujie@airwind.cn>
2015-08-11 23:02:25 +0800

    shader

--- openGL/project/openGl01/openGl01/GLUtils.h
+++ openGL/project/openGl01/openGl01/GLUtils.h
@@ -1,3 +1,4 @@
+#pragma once
 #include <windows.h>
 #ifndef GLEW_STATIC
 #define GLEW_STATIC
@@ -9,4 +10,13 @@
 #define FREEGLUT_STATIC
 #include <GL\glut.h>
 
+enum SHADER_ATTRIBUTE {ATTRIBUTE_VERTEX = 0,ATTRIBUTE_COLOR,ATTRIBUTE_NORMAL,ATTRIBUTE_TEXTURE0,ATTRIBUTE_TEXTURE1,ATTRIBUTE_TEXTURE2,ATTRIBUTE_TEXTURE3,ATTRIBUTE_LAST};
+enum PRESET_SHADER {SHADER_IDENTITY = 0,SHADER_FLAT,SHADER_LAST};
+GLuint presetShaders[SHADER_LAST];
+
 GLuint loadShaderPairSrcWithAttribute(const char* vertexSrc,const char* frameSrc, ...);
+
+void initPresetShaders();
+
+void usePresetShader(PRESET_SHADER shaderID,...);
+
